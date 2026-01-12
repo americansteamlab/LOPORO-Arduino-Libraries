d688babe77c826dfa2853d29fcc4455f49c0e719
americansteamlab <americansteamlab@gmail.com>
2026-01-11 23:58:07 -0800

    Update TB6612.h

--- src/TB6612.h
+++ src/TB6612.h
@@ -5,8 +5,15 @@ struct TB6612Pins {
   uint8_t AIN1, AIN2, PWMA;
   uint8_t BIN1, BIN2, PWMB;
   uint8_t STBY;        // 255 si no existe
-  bool invertA = false;
-  bool invertB = false;
+  bool invertA;
+  bool invertB;
+
+  TB6612Pins(uint8_t ain1, uint8_t ain2, uint8_t pwma,
+             uint8_t bin1, uint8_t bin2, uint8_t pwmb,
+             uint8_t stby = 255, bool invA = false, bool invB = false)
+  : AIN1(ain1), AIN2(ain2), PWMA(pwma),
+    BIN1(bin1), BIN2(bin2), PWMB(pwmb),
+    STBY(stby), invertA(invA), invertB(invB) {}
 };
 
 class TB6612 {
